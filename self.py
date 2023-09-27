da443fdc831ce6cf5ceb2277d8c6545130a736ce
FousiyaPH <fousiyanizarali@gmail.com>
2023-09-27 16:19:20 +0100

    error fixed

--- self.py
+++ self.py
@@ -4,7 +4,7 @@ class Employee:
         print("Name = ",self.name)
         #age is only accessible in this method
         age = 30
-        print("Age = ", age)
+        print("Age = ", self.age)
     def printEmployeeDetails(self):
         print("printing another method")
         print("Name: ",self.name)
@@ -13,7 +13,7 @@ class Employee:
         age attribute cannot be accessed within this method, because I haven't used the name of the object(self) to create the age attribute 
         in the employeeDetails method. so the method termintes this particular attribute cannot be used in any other methods.
         """
-        print("Age: ",age)
+        print("Age: ",self.age)
 
 employee = Employee()
 employee.employeeDetails() # Employee.employeeDetail(employee)- this statement shows how internally python makes this function call
