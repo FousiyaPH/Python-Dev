978a4329d0358df7916224644b4ff92f38989d50
FousiyaPH <fousiyanizarali@gmail.com>
2023-09-27 16:32:08 +0100

    error fixed and code clean

--- self.py
+++ self.py
@@ -2,17 +2,12 @@ class Employee:
     def employeeDetails(self):
         self.name = "James"
         print("Name = ",self.name)
-        #age is only accessible in this method
-        age = 30
-        print("Age = ", self.age)
+        self.age = 30
+        print("Age = ",self.age)
+
     def printEmployeeDetails(self):
         print("printing another method")
         print("Name: ",self.name)
-        """ 
-        will get error for the below statement as NameError: name 'age' is not defined
-        age attribute cannot be accessed within this method, because I haven't used the name of the object(self) to create the age attribute 
-        in the employeeDetails method. so the method termintes this particular attribute cannot be used in any other methods.
-        """
         print("Age: ",self.age)
 
 employee = Employee()
